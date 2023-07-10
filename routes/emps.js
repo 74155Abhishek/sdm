010bc2b8eab9a7a561a7d2de39808648b89886f4
74155Abhishek <abhishekghagare143@gmail.com>
2023-07-10 11:34:02 +0530

    from main1

--- routes/emps.js
+++ routes/emps.js
@@ -32,6 +32,75 @@ appForEmps.get("/", (request, response)=>{
 
 })
 
+appForEmps.post("/", (request, response)=>{
+    
+    var query = 
+    `insert into Employee_Tb values(${request.body.id}, '${request.body.e_name}','${request.body.email}', '${request.body.password}'),'${request.body.emp_id}','${request.body.dname}','${request.body.doj}'`;
+
+    connection.query(query, (error, result)=>{
+        if(error==null)
+        {
+            var data = JSON.stringify(result) 
+            response.setHeader("Content-Type","application/json");
+            response.write(data);
+        } 
+        else
+        {
+            console.log(error);
+            response.setHeader("Content-Type","application/json");
+            response.write(error)
+        }
+        response.end();
+})
+})
+
+
+appForEmps.put("/:id", (request, response)=>{
+    
+    var query = 
+    `update Emp set e_name = '${request.body.e_name}',
+                    password = '${request.body.password}' where id = ${request.params.ENo}`;
+
+    connection.query(query, (error, result)=>{
+                        if(error==null)
+                        {
+                            var data = JSON.stringify(result) 
+                            response.setHeader("Content-Type","application/json");
+                            response.write(data);
+                        } 
+                        else
+                        {
+                            console.log(error);
+                            response.setHeader("Content-Type","application/json");
+                            response.write(error)
+                        }
+                        response.end();
+                })
+})
+
+
+appForEmps.delete("/:id", (request, response)=>{
+   
+    var query = 
+    `delete from Emp where id = ${request.params.id}`;
+                    
+    connection.query(query, (error, result)=>{
+                        if(error==null)
+                        {
+                            var data = JSON.stringify(result) 
+                            response.setHeader("Content-Type","application/json");
+                            response.write(data);
+                        } 
+                        else
+                        {
+                            console.log(error);
+                            response.setHeader("Content-Type","application/json");
+                            response.write(error)
+                        }
+                        response.end();
+                })
+})
+
 
 
 module.exports = appForEmps;
